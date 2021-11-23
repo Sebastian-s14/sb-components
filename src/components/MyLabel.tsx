1bfc91d5e5d73188c6ead8a7d7bf59d733ac52e3
Sebastian <sebastian.sanchez@tecsup.edu.pe>
2021-11-23 16:59:19 -0500

    feat: end section

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,39 @@
+import './myLabel.css'
+
+export interface MyLabelProps {
+    /**
+        * Message to show
+    */
+    label: string
+    /**
+        * size button
+    */
+    size: 'normal' | 'h1' | 'h2' | 'h3'
+    /**
+        * All caps?
+    */
+    allCaps?: boolean
+    /**
+        * color
+    */
+    color?: 'primary' | 'secondary' | 'tertiary'
+    /**
+        * font color
+    */
+     fontColor?: string
+
+}
+
+export const MyLabel = ({
+    label = 'No Label',
+    size = 'normal',
+    allCaps = false,
+    color = 'primary',
+    fontColor
+}: MyLabelProps) => {
+    return (
+        <span className={`label ${ size } text-${color}`} style={{ color: fontColor }}>
+            { allCaps ? label.toUpperCase() : label }
+        </span>
+    )
+}
